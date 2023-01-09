5540077358f217a9735bc61146527f5eaad08f39
Sylvia Chen <sylvialchen33@gmail.com>
2023-01-09 14:46:33 -0500

    add jobs model, expand sisters model

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from hashlib import new
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.detail import DetailView
-from .models import Chapter, Sister, Pnm, Nickname_Request
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic.detail import DetailView
+from .models import Chapter, Sister, Pnm, Nickname_Request, Job_Opps_And_Referrals
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from .serializers import ChapterSerializer
+from .serializers import ChapterSerializer, JobOppsAndReferralsSerializer, SistersSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -33,3 +33,33 @@ def chapters_list(request):
         #     queryset = Chapter.objects.all()
 
         # Define the home view
+
+
+@api_view(['GET', 'POST'])
+def jobs_list(request):
+    if request.method == 'GET':
+        data = Job_Opps_And_Referrals.objects.all()
+        serializer = JobOppsAndReferralsSerializer(
+            data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = JobOppsAndReferralsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def sisters_list(request):
+    if request.method == 'GET':
+        data = Sister.objects.all()
+        serializer = SistersSerializer(
+            data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SistersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
