a53f18a52ae5b672eb88fff63358cc3bf886f7b3
Sylvia Chen <sylvialchen33@gmail.com>
2023-01-12 15:02:23 -0500

    adjust Chapter model, create sisters edit route

--- glo_management_backend/urls.py
+++ glo_management_backend/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 # Add the include function to the import
-from django.urls import path, include, re_path
+from django.urls import path, include
 from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # In this case '' represents the root route
     path('', include('main_app.urls')),
-    re_path(r'^api/chapters/$', views.chapters_list),
-    re_path(r'^api/jobs/$', views.jobs_list),
-    re_path(r'^api/sisters/$', views.sisters_list),
+    path('api/chapters/', views.chapters_list),
+    path('api/jobs/', views.jobs_list),
+    path('api/sisters/', views.sisters_list),
+    path('api/sisters/edit/<int:id>', views.sisters_detail),
 ]
