e27f5ac4faf4e0405913b8b14684f0d142927a5a
Sylvia Chen <sylvialchen33@gmail.com>
2023-01-19 10:36:56 -0500

    include type at end of models fields

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,35 +1,43 @@
 from rest_framework import serializers
-from .models import Chapter, Job_Opps_And_Referrals, Sister, Member_Experiences
+from .models import Chapter, Job_Opps_And_Referrals, Sister, Member_Experiences, Position_Titles
 
 
 class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapter
-        fields = ('id', 'associate_chapter', 'greek_letter_assigned', 'chapter_school',
-                  'city_state', 'original_founding_date', 'recharter_date', 'chapter_status')
+        fields = ('id', 'associate_chapter_fg', 'greek_letter_assigned_txt', 'chapter_school_txt',
+                  'city_state_txt', 'original_founding_date', 'recharter_date', 'chapter_status_txt')
 
 
 class MemberExperiencesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member_Experiences
-        fields = ('id', 'sister_member', 'position', 'start_date', 'end_date',
-                  'chapter')
+        fields = ('id', 'sister_nb', 'position_nb', 'start_date', 'end_date',
+                  'chapter_nb')
 
 
 class JobOppsAndReferralsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job_Opps_And_Referrals
-        fields = ('id', 'pub_date', 'job_title', 'company_name',
-                  'job_link', 'level_of_opening', 'industry', 'description', 'poster')
+        fields = ('id', 'pub_date', 'job_title_txt', 'company_name_txt',
+                  'job_link_txt', 'remote_role_fg', 'city_txt', 'state_txt',
+                  'level_of_opening_txt', 'industry_nb', 'description_txt', 'poster_nb')
+
+
+class PositionsTitlesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position_Titles
+        fields = ('id', 'position_title_txt', 'active_fg', 'e_board_fg',
+                  'description_txt', 'job_family_txt')
 
 
 class SistersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sister
-        fields = ('id', 'first_name', 'last_name', 'nickname',
-                  'nickname_meaning', 'chapter', 'crossing_chapter',
-                  'crossing_class', 'crossing_date', 'initiation_date',
-                  'line_number', 'big_sister', 'tree', 'status',
-                  'current_city', 'current_state', 'current_country',
-                  'email_address', 'coach', 'current_position', 'current_company',
-                  'linkedin_url', 'expertise_interests', 'summary')
+        fields = ('id', 'first_name_txt', 'last_name_txt', 'nickname_txt',
+                  'nickname_meaning_txt', 'chapter_nb', 'crossing_chapter_nb',
+                  'crossing_class_txt', 'crossing_date', 'initiation_date',
+                  'line_nb', 'big_sister_nb', 'tree_txt', 'status_txt',
+                  'current_city_txt', 'current_state_txt', 'current_country_txt',
+                  'email_address_txt', 'coach_fg', 'current_position_txt', 'current_company_txt',
+                  'linkedin_url_txt', 'expertise_interests_nb', 'summary_txt')
