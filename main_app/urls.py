d24932c007914494004419ef588bf40097a2ca89
Sylvia Chen <sylvialchen33@gmail.com>
2023-03-20 15:51:24 -0400

    expand user/me to return object of member who is logged in

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("api/experiences", views.experiences_list),
     path("api/coaches", views.coach_list),
     path("api/events/", views.events_list),
-    # path("user/me/", views.ExtendedUserMe, name="authemail-me"),
+    path("user/me/", views.extended_user_me),
 ]
